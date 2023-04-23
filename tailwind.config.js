e442132c96051a40bbed0979090b64ada54525bb
fl1p <b21fa1739@ufe.edu.mn>
2023-04-24 06:19:44 +0800

    asd

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#101010",
+        gray: "#1b1b1b",
+        light: "#a1a1a1",
+      },
+    },
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
+  },
+  plugins: [],
+};
